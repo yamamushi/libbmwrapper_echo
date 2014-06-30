bd16e1c1adfcc8d159e99c204f2d75ba88b05ecd
yamamushi <yamamushi@gmail.com>
2014-06-30 17:40:46 +0200

    Base

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  bmecho
+//
+//  Created by Jonathan Rumion on 6/30/14.
+//
+//
+
+#include <iostream>
+
+int main(){
+    
+    std::cout << "Test" << std::endl;
+    
+    return 0;
+}
